ae94b2b5e06d63c5db8d53f376cc9ce05a86fcc5
Yuan Huang <huangyuan3h@gmail.com>
2024-02-28 17:58:54 +0800

    update the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,32 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
-
-const san = Open_Sans({subsets:["latin"]});
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
+const san = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "北美直连",
-  description: "帮你选择加拿大移民项目和机构",
-  applicationName: "北美直连",
-  authors: { name: "Yuan Huang", url: "https://github.com/huangyuan3h" },
-  keywords: ["加拿大","移民"],
-
+  title: '北美直连',
+  description: '帮你选择加拿大移民项目和机构',
+  applicationName: '北美直连',
+  authors: { name: 'Yuan Huang', url: 'https://github.com/huangyuan3h' },
+  keywords: ['加拿大', '移民'],
+  viewport: { width: 'device-width', initialScale: 1 },
+  icons: [
+    { url: '/favicon.ico' },
+    { url: '/apple-touch-icon.png', rel: 'apple-touch-icon', sizes: '180x180' },
+    {
+      url: '/favicon-32x32.png',
+      rel: 'icon',
+      sizes: '32x32',
+      type: 'image/png',
+    },
+    {
+      url: '/favicon-16x16.png',
+      rel: 'icon',
+      sizes: '16x16',
+      type: 'image/png',
+    },
+  ],
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
