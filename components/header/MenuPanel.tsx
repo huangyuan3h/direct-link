963ff13273ce56879ed2bb15c90f6b930d09b16e
Yuan Huang <huangyuan3h@gmail.com>
2024-04-12 14:35:54 +0800

    update the menu config

--- components/header/MenuPanel.tsx
+++ components/header/MenuPanel.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useUser } from '../user-context';
-import { Button, ListGroup, Offcanvas } from 'react-bootstrap';
+import { Accordion, Button, ListGroup, Offcanvas } from 'react-bootstrap';
 import { Avator } from './Avator';
+import { menuConfig } from './menuConfig';
 
 export interface MenuPanelProps {
   showPanel: boolean;
@@ -20,12 +21,28 @@ export const MenuPanel: React.FC<MenuPanelProps> = ({
         </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        <ListGroup variant="flush">
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        </ListGroup>
+        <Accordion defaultActiveKey="0" flush>
+          {menuConfig.map((menu, idx) => {
+            return (
+              <Accordion.Item key={`menu-${menu.key}`} eventKey={`${idx}`}>
+                <Accordion.Header>{menu.title}</Accordion.Header>
+                <Accordion.Body>
+                  <ListGroup variant="flush">
+                    {menu.children.map((c) => {
+                      return (
+                        <ListGroup.Item key={`menu-child-${c.key}`}>
+                          <Button variant="link" href={c.url}>
+                            {c.title}
+                          </Button>
+                        </ListGroup.Item>
+                      );
+                    })}
+                  </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
       </Offcanvas.Body>
     </Offcanvas>
   );
