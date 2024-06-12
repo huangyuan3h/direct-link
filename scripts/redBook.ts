ca81959148f3301ab33fe4c8e2d5ac6648f05459
Yuan Huang <huangyuan3h@gmail.com>
2024-06-12 16:34:26 +0800

    change model to flash

--- scripts/redBook.ts
+++ scripts/redBook.ts
@@ -173,7 +173,7 @@ const generatePayload = async (text: string): Promise<GeneratedPostType> => {
   };
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${process.env.GEMINI_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_KEY}`,
     {
       method: 'POST',
       body: JSON.stringify(prompts),
