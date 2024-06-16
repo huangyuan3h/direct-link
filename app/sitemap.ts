30807e572bdcaa732cfbe6140dcc599d33911a27
Yuan Huang <huangyuan3h@gmail.com>
2024-06-16 21:42:54 +0800

    update the site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: `${DOMAIN_URL}about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
   ];
 
   // add list page
